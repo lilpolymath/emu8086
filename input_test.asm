4819caeb91a502c3a848066b516681104749323d
Favour <ayofavour7@gmail.com>
2019-05-20 13:11:32 +0100

    Create input_test.asm
    Demonstrates how input works with.

---
+++ input_test.asm
@@ -0,0 +1,26 @@
+include 'emu8086.inc'
+org 100h
+
+LEA    SI, msg1       ; ask for the number
+CALL   print_string   ;
+CALL   scan_num       ; get number in CX.
+
+MOV    AX, CX         ; copy the number to AX.
+
+; print the following string:
+CALL   pthis
+DB  13, 10, 'You have entered: ', 0
+
+CALL   print_num      ; print number in AX.
+
+RET                   ; return to operating system.
+
+msg1   DB  'Enter the number: ', 0
+
+DEFINE_SCAN_NUM
+DEFINE_PRINT_STRING
+DEFINE_PRINT_NUM
+DEFINE_PRINT_NUM_UNS  ; required for print_num.
+DEFINE_PTHIS
+
+END       
